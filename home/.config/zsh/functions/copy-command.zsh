5444d24803ac348b214150f498696c519de05399
vah <148314747+vahrina@users.noreply.github.com>
2026-09-21 22:22:40 +0200

    zsh: remove unnecessary functions

--- home/.config/zsh/functions/copy-command.zsh
+++
@@ -1,6 +0,0 @@
-copy-command() {
-  echo -n $BUFFER | xclip -selection clipboard
-  zle -M "copied to clipboard"
-}
-zle -N copy-command
-bindkey '^Xc' copy-command
--- home/.config/zsh/functions/rcpri.zsh
+++
@@ -1,4 +0,0 @@
-rcpri() {
-  git remote remove origin 2>/dev/null
-  gh repo create "$(basename $PWD)" --private --source=. --remote=origin --push
-}
